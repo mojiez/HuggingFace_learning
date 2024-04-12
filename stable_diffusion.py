7a3792f617b0f018e48e5f859493a588ce04a858
mojiez <937691466@qq.com>
2024-04-12 18:49:57 +0800

    SD模型生成图像 图像未保存 待解决

--- stable_diffusion.py
+++ stable_diffusion.py
@@ -1,6 +1,9 @@
 from diffusers import DiffusionPipeline
 
 if __name__ == '__main__':
-    # 用from_pretrained()方法加载模型。
+    # 用from_pretrained()方法加载模型 SD
     pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
-    print(pipeline)
+    # print(pipeline)
+    # DiffusionPipeline会下载并缓存所有的建模、标记化和调度组件。你可以看到StableDiffusion的pipeline是由UNet2DConditionModel和PNDMScheduler等组件组成的：
+    image = pipeline("superman").images[0]
+    image.save("superman.png")
